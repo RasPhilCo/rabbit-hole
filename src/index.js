c7c1e759186943cfce25f821b8b031ac4eb722b8
RasPhilCo <RasPhilCo@users.noreply.github.com>
2017-08-30 00:45:45 -0700

    add rabbit-hole

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-// @flow
+#! /usr/bin/env node
 
-import fs from 'fs-extra'
-import path from 'path'
+var fs = require('fs-extra');
+var path = require('path');
+var treeify = require('treeify');
 
-export const topic = {
-  name: 'hello',
-  description: 'says hello (example plugin)'
+var argv = process.argv.slice(2)
+
+var short = []
+for (var i in argv) {
+  var arg = argv[i]
+  if (arg.match(/^-\w+/)) {
+    short = short.concat(arg.split('').filter(a => a !== '-'))
+  }
+}
+argv = argv.concat(short)
+
+var rootOnly = argv.includes('--only-path') || argv.includes('p')
+var hideLink = argv.includes('--hide-links') || argv.includes('h')
+
+var locales = process.env.PATH.split(':')
+let tree = {}
+for (var i in locales) {
+  var locale = locales[i]
+  tree[locale] = {}
+  if (!rootOnly){
+    var links = fs.readdirSync(locale)
+    for (var j in links) {
+      let symlink = null
+      try {
+        symlink = fs.readlinkSync(`${locale}/${links[j]}`)
+        symlink = path.resolve(symlink)
+      } catch (err) {
+        // we only want symlinks
+        continue
+      }
+      tree[locale][links[j]] = hideLink ? null : symlink
+    }
+  }
 }
 
-let dir = path.join(__dirname, 'commands')
-export const commands = fs.readdirSync(dir)
-  .filter(f => path.extname(f) === '.js' && !f.endsWith('.test.js'))
-  // $FlowFixMe
-  .map(f => require('./commands/' + f).default)
+console.log(treeify.asTree(tree, true))
